d6e361dec03cc56b95d3565c2fdb07b2f389d82a
SENA <SENA@ARMSCCCCTGOD171.FormacionSena.RED>
2024-10-21 16:48:44 -0500

    PROGRESS

--- app.js
+++ app.js
@@ -1,54 +1,66 @@
-const audio = document.getElementById(`audio`)
-const play = document.getElementById(`play`)
-const pause = document.getElementById(`pause`)
-const forward = document.getElementById(`forward`)
-const rewind = document.getElementById(`rewind`)
-const stop = document.getElementById(`stop`)
+const audio = document.getElementById('audio');
+const play = document.getElementById('play');
+const pause = document.getElementById('pause');
+const forward = document.getElementById('forward');
+const rewind = document.getElementById('rewind');
+const stop = document.getElementById('stop');
 
-
-play.addEventListener(`click`, () => audio.play ())
-
-pause.addEventListener(`click`,() => audio.pause ())
-
-rewind.addEventListener(`click`,() => audio.currentTime -= 10)
-
-forward.addEventListener(`click`,() => audio.currentTime += 10)
-
-stop.addEventListener(`click`, () => {
-    audio.pause ()
-    audio.currentTime = 0
-})
-
+play.addEventListener('click', () => audio.play());
+pause.addEventListener('click', () => audio.pause());
+rewind.addEventListener('click', () => audio.currentTime -= 10);
+forward.addEventListener('click', () => audio.currentTime += 10);
+stop.addEventListener('click', () => {
+    audio.pause();
+    audio.currentTime = 0;
+});
 
 const playList = [
     {
-      title: 'bonita',
-      artist: 'Arcangel',
-      img: 'images\arcangel.jpg',
-      song: 'music\y2mate.com - Bonita.mp3'
+        title: 'Bonita',
+        artist: 'Arcangel',
+        img: 'images/arcangel.jpg',
+        song: 'music/y2mate.com - Bonita.mp3'
     },
     {
-      title: 'y si la ves ',
-      artist: 'ñejo',
-      img: 'images\ñejo.png',
-      song: 'music\y2mate.com - ÑEJO  Y SI LA VES.mp3'
+        title: 'Y Si La Ves',
+        artist: 'Ñejo',
+        img: 'images/ñejo.png',
+        song: 'music/y2mate.com - ÑEJO Y SI LA VES.mp3'
     },
     {
-      title: 'Song 3',
-      artist: 'Artist 3',
-      img: 'ruta de la imagen de la canción',
-      song: 'ruta de la cancion'
+        title: 'Mi Fanática',
+        artist: 'Arcangel',
+        img: 'images/arcangel.jpg',
+        song: 'music/y2mate.com - arcangel mi fanatica letra.mp3'
     },
     {
-      title: 'Song 4',
-      artist: 'Artist 4',
-      img: 'ruta de la imagen de la canción',
-      song: 'ruta de la cancion'
+        title: 'Las Avispas',
+        artist: 'Juan Luis Guerra',
+        img: 'images/Juan Luis Guerra.png',
+        song: 'music/y2mate.com - Las Avispas.mp3'
     },
     {
-      title: 'Song 5',
-      artist: 'Artist 5',
-      img: 'ruta de la imagen de la canción',
-      song: 'ruta de la cancion'
+        title: 'Oh Qué Será?',
+        artist: 'Willie Colón',
+        img: 'images/willie colon.png',
+        song: 'music/y2mate.com - Oh Qué Será.mp3'
     },
-  ]
+];
+let currentSongIndex = 0; 
+
+
+function playSong(song) {
+    const audio = document.getElementById("audio");
+    audio.src = song.src; 
+    audio.play(); 
+}
+
+
+function nextSong() {
+    currentSongIndex++; 
+    if (currentSongIndex >= playList.length) {
+        currentSongIndex = 0; 
+    }
+    playSong(playList[currentSongIndex]); 
+}
+document.getElementById("next").addEventListener("click", nextSong);
